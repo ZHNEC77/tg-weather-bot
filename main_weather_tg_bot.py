af8b5b151ce5ae042dafe52626c1c9cac0991246
ZHNEC77 <vadim030714@gmail.com>
2024-08-19 12:06:09 +0300

    some fix

--- main_weather_tg_bot.py
+++ main_weather_tg_bot.py
@@ -3,21 +3,22 @@ import asyncio
 import requests
 import datetime
 from dotenv import load_dotenv
-from aiogram import Bot, Dispatcher, types, F
+from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
-from main import get_weather
+
 
 load_dotenv()
 dp = Dispatcher()
 
+
 @dp.message(CommandStart())
 async def start_command(message: types.Message):
-    await message.answer('Привет, крипочек! Напиши мне название города и я пришлю сводку погоды!')
+    await message.answer('Привет, Напиши мне название города и я пришлю сводку погоды!')
 
 
 @dp.message()
 async def get_weather1(message: types.Message):
-        
+
     code_to_smile = {
         "Clear": "Ясно \U00002600",
         "Clouds": "Облачно \U00002601",
@@ -27,13 +28,12 @@ async def get_weather1(message: types.Message):
         "Snow": "Снег \U0001F328",
         "Mist": "Туман \U0001F32B"
     }
-    
+
     try:
         r = requests.get(
             f"https://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={os.getenv('WEATHER_TOKEN')}&units=metric"
-    )
+        )
         data = r.json()
-
 
         city = data['name']
         cur_weather = data['main']['temp']
@@ -45,20 +45,21 @@ async def get_weather1(message: types.Message):
         humidity = data['main']['humidity']
         pressure = data['main']['pressure']
         wind = data['wind']['speed']
-        sunrise_timestamp = datetime.datetime.fromtimestamp(data['sys']['sunrise'])
-        sunset_timestamp = datetime.datetime.fromtimestamp(data['sys']['sunset'])
+        sunrise_timestamp = datetime.datetime.fromtimestamp(
+            data['sys']['sunrise'])
+        sunset_timestamp = datetime.datetime.fromtimestamp(
+            data['sys']['sunset'])
         lenth_of_the_day = datetime.datetime.fromtimestamp(data['sys']['sunset']) - datetime.datetime.fromtimestamp(
             data['sys']['sunrise'])
-        
+
         await message.reply(f"***{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}***\n"
-              f"Погода в городе: {city}\nТемпература: {cur_weather}С° {wd}\n"
-              f"Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст\nВетер: {wind} м/с\n"
-              f"Восход солнца: {sunrise_timestamp}\nЗакат солнца: {sunset_timestamp}\nПродолжительность дня: {lenth_of_the_day}\n"
-              f"***Хорошего дня, а Дима скуф !***"
-              )
+                            f"Погода в городе: {city}\nТемпература: {cur_weather}С° {wd}\n"
+                            f"Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст\nВетер: {wind} м/с\n"
+                            f"Восход солнца: {sunrise_timestamp}\nЗакат солнца: {sunset_timestamp}\nПродолжительность дня: {lenth_of_the_day}\n"
+                            f"***Хорошего дня !***"
+                            )
     except:
         await message.reply('\U00002620 Проверьте название города \U00002620')
-    
 
 
 async def main():
